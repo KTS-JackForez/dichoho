83d771f0daf8ff879014f871401b6a7baaa90723
Jack <jackforez@gmail.com>
2023-01-11 00:21:14 +0700

    tạo fronted

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <div className=" flex justify-center">ok</div>;
+}
+
+export default App;
