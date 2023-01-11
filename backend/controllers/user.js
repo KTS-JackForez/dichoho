b2ca04c7b802f8a0b21c4aab186db8b3120ad067
Jack <jackforez@gmail.com>
2023-01-11 22:52:55 +0700

    done userController

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,50 +1,94 @@
 import { createError } from "../error.js";
 import User from "../models/User.js";
-
+const permission = ["admin", "staff"];
 //get một user
 export const getUser = async (req, res, next) => {
   try {
-    res.status(200).json("getUser");
+    const user = await User.findById(req.params.id).select(["-password"]);
+    if (!user) {
+      return res.status(404).json("Không tìm thấy thông tin người dùng");
+    }
+    res.status(200).json(user);
   } catch (error) {
     next(error);
   }
 };
 //get nhiều user, áp dụng cho admin/staff
 export const getUsers = async (req, res, next) => {
+  if (!permission.includes(req.user.role)) {
+    // Chỉ user/staff mới được phép thực hiện chức năng này
+    return res.status(403).json("Tham số truyền vào không đúng");
+  }
   try {
-    res.status(200).json("getUserssssssss");
+    const users = await User.find().select(["-password"]);
+    res.status(200).json(users);
   } catch (error) {
     next(error);
   }
 };
 //get user theo số phone
 export const getByPhone = async (req, res, next) => {
+  if (!permission.includes(req.user.role)) {
+    // Chỉ user/staff mới được phép thực hiện chức năng này
+    return res
+      .status(403)
+      .json("Bạn không được cấp quyền thực hiện chức năng này");
+  }
   try {
-    res.status(200).json("getByPhone");
+    const users = await User.find({ phone: req.params.phone }).select([
+      "-password",
+    ]);
+    res.status(200).json(users);
   } catch (error) {
     next(error);
   }
 };
 //update thông tin user
 export const updateUser = async (req, res, next) => {
+  if (!req.params.id === req.user.id && permission.includes(req.user.role)) {
+    return next(
+      createError(
+        403,
+        "Bạn chỉ được phép cập nhật thông tin tài khoản của mình"
+      )
+    );
+  }
   try {
-    res.status(200).json("updateUser");
+    await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json("Cập nhật thông tin tài khoản thành công");
   } catch (error) {
     next(error);
   }
 };
 //update quyền của user
 export const updateUserRole = async (req, res, next) => {
+  const role = process.env.ROLE;
+  if (!role.includes(req.user.role) && req.body.new_role !== "admin") {
+    return next(createError(403, "Tham số truyền lên không đúng"));
+  }
   try {
-    res.status(200).json("updateUserRole");
+    await User.findByIdAndUpdate(req.body.userID, {
+      $set: { role: req.body.new_role },
+    });
+    res.status(200).json("Cập nhật quyền user thành công");
   } catch (error) {
     next(error);
   }
 };
 // delete một user
 export const deleteUser = async (req, res, next) => {
+  if (!permission.includes(req.user.role)) {
+    return next(
+      createError(403, "Bạn không được phép thực hiện chức năng này")
+    );
+  }
   try {
-    res.status(200).json("deleteUser");
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User đã bị xóa");
   } catch (error) {
     next(error);
   }
