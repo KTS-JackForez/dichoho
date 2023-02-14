b1998ac40a50fe3fcd3fad8f562f58f1965e2302
Jack <jackforez@gmail.com>
2023-02-14 13:38:29 +0700

    v

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import promo from "../assets/imgs/promo.jpg";
 const Slider = () => {
   const dots = [1, 2, 3, 4];
+  const [activeSlide, setActiveSlide] = useState(0);
+  const data = [""];
   return (
     //wrapper
     <div className="w-full bg-red-400 bottom-0 mt-1">
       <div className="max-w-screen-xl mx-auto">
         <div className="relative">
           <img src={promo} alt="" />
+
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -15,6 +18,11 @@ const Slider = () => {
             strokeWidth={1.5}
             stroke="currentColor"
             className="w-8 h-8 absolute top-[50%] text-gray-600 hover:text-black cursor-pointer hover:bg-gray-200 rounded-full"
+            onClick={() => {
+              activeSlide > 0
+                ? setActiveSlide((activeSlide) => activeSlide - 1)
+                : setActiveSlide(3);
+            }}
           >
             <path
               strokeLinecap="round"
@@ -22,6 +30,7 @@ const Slider = () => {
               d="M15.75 19.5L8.25 12l7.5-7.5"
             />
           </svg>
+
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -29,6 +38,11 @@ const Slider = () => {
             strokeWidth={1.5}
             stroke="currentColor"
             className="w-8 h-8 absolute top-[50%] right-0 text-gray-600 hover:text-black cursor-pointer hover:bg-gray-200 rounded-full"
+            onClick={() => {
+              activeSlide < 3
+                ? setActiveSlide((activeSlide) => activeSlide + 1)
+                : setActiveSlide(0);
+            }}
           >
             <path
               strokeLinecap="round"
@@ -38,9 +52,16 @@ const Slider = () => {
           </svg>
           <div className=" absolute bottom-1 w-full">
             <div className="flex justify-center">
-              {dots.map((i) => {
+              {dots.map((i, index) => {
                 return (
-                  <button className="hover:bg-white bg-white/20 mx-1 px-2 rounded-full">
+                  <button
+                    className={`${
+                      activeSlide === index
+                        ? "bg-white"
+                        : "hover:bg-white bg-white/20"
+                    }  mx-1 px-2 rounded-full`}
+                    onClick={() => setActiveSlide(i - 1)}
+                  >
                     {i}
                   </button>
                 );
