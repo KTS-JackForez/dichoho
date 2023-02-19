37d523fd599173e16695bb4282fa39622a183200
JackForez <jackforez@gmail.com>
2023-02-19 21:15:23 +0700

    v

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,8 +26,8 @@ const MyCart = ({
 };
 const Footer = () => {
   return (
-    <div className="w-full">
-      <div className="max-w-screen-xl mx-auto flex flex-wrap justify-around">
+    <div className="w-full bg-green-100">
+      <div className="max-w-screen-xl mx-auto flex flex-wrap justify-between pt-3">
         <MyCart
           img="https://green.web5phut.com/wp-content/uploads/2022/07/p1.png"
           text1="sản phẩm"
@@ -53,32 +53,33 @@ const Footer = () => {
           size="56"
         />
       </div>
-      <div className="flex gap-2">
-        <div>
+      <div className="flex justify-between mt-2 mx-auto max-w-screen-xl p-3">
+        <div className="flex gap-3 flex-1">
           <img src={logo} alt="" />
-          <ul>
+          <ul className="pt-8">
             <li>Địa chỉ</li>
             <li>Email</li>
-            <li>Điện thoại</li>
+            <li className="inline">Điện thoại</li>
           </ul>
         </div>
-        <div>
-          <h3>về chúng tôi</h3>
-          <ul>
-            <li>Địa chỉ</li>
-            <li>Email</li>
-            <li>Điện thoại</li>
-          </ul>
+        <div className="flex gap-5 py-8 w-1/3">
+          <div>
+            <h3 className="uppercase font-semibold">về chúng tôi</h3>
+            <ul className="gap-3">
+              <li>Địa chỉ</li>
+              <li>Email</li>
+              <li>Điện thoại</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="uppercase font-semibold">chính sách</h3>
+            <ul className="gap-3">
+              <li>Địa chỉ</li>
+              <li>Email</li>
+              <li>Điện thoại</li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <h3>chính sách</h3>
-          <ul>
-            <li>Địa chỉ</li>
-            <li>Email</li>
-            <li>Điện thoại</li>
-          </ul>
-        </div>
-        <div></div>
       </div>
       <div className=" bg-green-600 mx-auto py-3 text-white font-semibold text-center">
         Copyright 2023 © Thiết kế bởi sale168.com
