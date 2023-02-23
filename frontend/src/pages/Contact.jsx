eba5538af6c290dff937a9f844defd5c284d5670
JackForez <jackforez@gmail.com>
2023-02-23 12:22:01 +0700

    v

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Footer, Header, Navbar, Promotion } from "../components";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return  <div className="bg-gray-100">
+    
+  <div className="max-w-screen-xl bg-white mx-auto">
+  <Promotion />
+   <Header />
+   <Navbar />
+    <div>
+    đây là content của page contact
+    </div>
+   <Footer />
+  </div>
+ 
+ </div>;
 };
 
 export default Contact;
