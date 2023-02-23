43d3cac52ce2ef7421a941d9e57bb4847e971ddc
Tuan <dinhxuantuan2001@gmail.com>
2023-02-23 14:26:58 +0700

    t

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from "./Home";
 export { default as News } from "./News";
-export { default as Products } from "./Products";
+export { default as Products } from "./products";
 export { default as Product } from "./Product";
 export { default as Contact } from "./Contact";
 export { default as NotFound } from "./NotFound";
