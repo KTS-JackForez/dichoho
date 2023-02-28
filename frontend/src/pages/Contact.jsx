394e44b0a1ec4eafa1a0dbe7135acae36a4b84da
Tuan <dinhxuantuan2001@gmail.com>
2023-02-28 11:03:22 +0700

    t

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Footer, Header, Navbar, Promotion } from "../components";
+import './Contact.css';
+import '../assets/css/base.css';
 
 const Contact = () => {
   return  <div className="bg-gray-100">
@@ -8,9 +10,53 @@ const Contact = () => {
   <Promotion />
    <Header />
    <Navbar />
-    <div>
-    đây là content của page contact
+   <div class="main">
+
+<div class="section-content">
+    <div class="grid">
+        <div class="contact-content">
+            <div class="grid__row">
+                <div class="contact-content-introduce">
+                    <h3>GREEN FOOD</h3>
+                    <p>Mọi thắc mắc quý khách vui lòng liên hệ tới chúng tôi thông qua thông tin bên dưới
+                        hoặc quý khách có thể điền thông tin ở form bên cạnh. Chúng tôi sẽ liên hệ giải đáp
+                        thắc mắc tới quý khách một cách sớm nhất.</p>
+                    <p><i class="fa fa-map-marker" aria-hidden="true"></i> Địa chỉ : FLC Star 418 Quang
+                        Trung, La Khê, Hà Đông</p>
+                    <p><i class="fa fa-envelope" aria-hidden="true"></i> Email : greenfood@gmail.com</p>
+                    <p><i class="fa fa-phone" aria-hidden="true"></i> Điện thoại: 0387969609</p>
+                    {/* <p><img decoding="async" loading="lazy" class="alignnone size-full wp-image-400"
+                            src={tenanh} alt="" width="232" height="89"/></p> */}
+
+                </div>
+                <div class="contact-content-information">
+                        <span>
+                            <input type="text" name="" id="" placeholder="Họ và tên..."/>
+                            <span></span>
+                        </span>
+                        <span>
+                            <input type="email" name="" id="" placeholder="Địa chỉ email..."/>
+                            <span></span>
+                        </span>
+                        <span>
+                            <input type="tel" name="" id="" placeholder="Số điện thoại..."/>
+                            <span></span>
+                        </span>
+                        <span>
+                            <textarea name="" id="" cols="40" rows="10" placeholder="Nhập nội dung liên hệ..."></textarea>
+                        </span>
+                        <input type="submit" value="GỬI LIÊN HỆ" class="btn btn--primary"/>
+                </div>
+
+            </div>
+        </div>
+
     </div>
+
+
+</div>
+
+</div>
    <Footer />
   </div>
  
--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,7 +1,173 @@
 import React from "react";
+import './News.css';
+import '../assets/css/base.css';
 
 const News = () => {
-  return <div>News</div>;
+  return  <div class="main">
+
+  <div class="news">
+      <div class="news-main grid">
+          <div class="news-content">
+              <div class="grid__column-9">
+                  <div class="grid__row">
+                  <div class="grid__column-3-9">
+                          <div class="post-item">
+                              <div>
+                                  <img class="post-item__img"
+                                    />
+
+                              </div>
+
+                              <h5 class="post-item__name">
+                                  10 loại rau củ quả tốt cho cơ thể
+                              </h5>
+                              <div class="post-item__date">
+                                  <span class="post-item__date-current">Tháng Tám, 2022</span>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="grid__column-3-9">
+                          <div class="post-item">
+                              <div>
+                                  <img class="post-item__img"
+                                    />
+
+                              </div>
+
+                              <h5 class="post-item__name">
+                                  10 loại rau củ quả tốt cho cơ thể
+                              </h5>
+                              <div class="post-item__date">
+                                  <span class="post-item__date-current">Tháng Tám, 2022</span>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="grid__column-3-9">
+                          <div class="post-item">
+                              <div>
+                                  <img class="post-item__img"
+                                    />
+
+                              </div>
+
+                              <h5 class="post-item__name">
+                                  10 loại rau củ quả tốt cho cơ thể
+                              </h5>
+                              <div class="post-item__date">
+                                  <span class="post-item__date-current">Tháng Tám, 2022</span>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="grid__column-3-9">
+                          <div class="post-item">
+                              <div>
+                                  <img class="post-item__img"
+                                    />
+
+                              </div>
+
+                              <h5 class="post-item__name">
+                                  10 loại rau củ quả tốt cho cơ thể
+                              </h5>
+                              <div class="post-item__date">
+                                  <span class="post-item__date-current">Tháng Tám, 2022</span>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="grid__column-3-9">
+                          <div class="post-item">
+                              <div>
+                                  <img class="post-item__img"
+                                    />
+
+                              </div>
+
+                              <h5 class="post-item__name">
+                                  10 loại rau củ quả tốt cho cơ thể
+                              </h5>
+                              <div class="post-item__date">
+                                  <span class="post-item__date-current">Tháng Tám, 2022</span>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="grid__column-3-9">
+                          <div class="post-item">
+                              <div>
+                                  <img class="post-item__img"
+                                    />
+
+                              </div>
+
+                              <h5 class="post-item__name">
+                                  10 loại rau củ quả tốt cho cơ thể
+                              </h5>
+                              <div class="post-item__date">
+                                  <span class="post-item__date-current">Tháng Tám, 2022</span>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="grid__column-3">
+                  <div>
+                      <div class="news-cmtt">
+                          <h3>CHUYÊN MỤC TIN TỨC</h3>
+                      </div>
+                      <ul class="news-menu">
+                          <li class="news-menu-item news-menu-item-seperate">
+                              <a href="">Thời trang & cuộc sống</a>
+                          </li>
+                          <li class="news-menu-item news-menu-item-seperate">
+                              <a href="">Tin công nghệ</a>
+                          </li>
+                          <li class="news-menu-item news-menu-item-seperate">
+                              <a href="">Tin tức</a>
+                          </li>
+                          <li class="news-menu-item news-menu-item-seperate">
+                              <a href="">Tuyển dụng</a>
+                          </li>
+                      </ul>
+                  </div>
+                  <div>
+                      <div class="news-ttmn">
+                          <h3>TIN TỨC MỚI NHẨT</h3>
+                      </div>
+                      <div class="news-blog">
+                          <div class="blog-post">
+                              {/* <img src="./assets/imgs/image/1-1-280x240.webp" alt=""/> */}
+                              <img src="https://via.placehoder.com/300.png/09f/fff" alt=""/>
+                              <a href="" title="10 loại rau củ quả tốt cho cơ thể">10 loại rau củ quả tốt cho cơ thể</a>
+                          </div>
+                          <div class="blog-post">
+                          {/* <img src="./assets/imgs/image/1-1-280x240.webp" alt=""/> */}
+                              <a href="" title="10 loại rau củ quả tốt cho cơ thể">10 loại rau củ quả tốt cho cơ thể</a>
+                          </div>
+                          <div class="blog-post">
+                          {/* <img src="./assets/imgs/image/1-1-280x240.webp" alt=""/> */}
+                              <a href="" title="10 loại rau củ quả tốt cho cơ thể">10 loại rau củ quả tốt cho cơ thể</a>
+                          </div>
+                          <div class="blog-post">
+                          {/* <img src="./assets/imgs/image/1-1-280x240.webp" alt=""/> */}
+                              <a href="" title="10 loại rau củ quả tốt cho cơ thể">10 loại rau củ quả tốt cho cơ thể</a>
+                          </div>
+                          <div class="blog-post">
+                          {/* <img src="./assets/imgs/image/1-1-280x240.webp" alt=""/> */}
+                              <a href="" title="10 loại rau củ quả tốt cho cơ thể">10 loại rau củ quả tốt cho cơ thể</a>
+                          </div>
+                      </div>
+                  </div>
+                
+              </div>
+
+          </div>
+
+      </div>
+   
+
+
+  </div>
+</div>
+;
 };
 
 export default News;
