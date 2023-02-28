0601bb1d7cf3fa7d0a5865c29744f06b8463ebac
Jack <jackforez@gmail.com>
2023-02-28 13:04:11 +0700

    git OK - v

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,4 +8,4 @@ export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 export { default as About } from "./About";
 export { default as Cart } from "./Cart";
-// import '@fortawesome/fontawesome-free/css/all.min.css';
+// git OK;
