6a7055ad27b575f2f1888c23ef8ee67864f192b0
Jack <jackforez@gmail.com>
2023-03-03 12:00:27 +0700

    v

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -9,12 +9,17 @@ import {
 } from "../controllers/post.js";
 import { verifyToken } from "../verifyToken.js";
 const router = express.Router();
-
+//lấy toàn bộ bài viết
 router.get("/", getPosts);
+//lấy bài viết cụ thể theo id
 router.get("/:id", getPost);
+//lấy bài viết theo id sản phẩm
 router.get("/product", getPostByProductId);
+//tạo mới bài viết
 router.post("/", verifyToken, createPost);
+//cập nhật bài viết
 router.put("/:id", verifyToken, editPost);
+//xóa bài viết
 router.get("/:id", verifyToken, deletePost);
 
 export default router;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -18,18 +18,31 @@ import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+//lấy toàn bộ sản phẩm
 router.get("/", getProducts);
+//lấy sản phẩm được tạo bởi user
 router.get("/my", verifyToken, getMyProducts);
+//lấy ra sản phẩm cụ thể theo id
 router.get("/:id", getProduct);
+//lấy ra sản phẩm the tags/categories
 router.get("/tags", verifyToken, getProductByTag);
+//thêm một like cho sản phẩm
 router.get("/addlike", verifyToken, addLike);
+//bớt một like cho sản phẩm
 router.get("/sublike", verifyToken, subLike);
+//tìm kiếm sản phẩm theo từ khóa
 router.get("/search", verifyToken, search);
+//cập nhật thông tin sản phẩm
 router.put("/:id", verifyToken, updateProduct);
+//cập nhật giá cho sản phẩm
 router.put("/:id", verifyToken, updatePrice);
+//thêm tag/category cho sản phẩm
 router.post("/addtag", verifyToken, addTag);
+//xóa tag/category cho sản phẩm
 router.post("/remove", verifyToken, removeTag);
+//tạo mới sản phẩm
 router.post("/", verifyToken, createProduct);
+//xóa sản phẩm
 router.delete("/:id", verifyToken, deleteProduct);
 
 export default router;
