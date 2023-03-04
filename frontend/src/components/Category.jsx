da8f187265745cd84987bd1f3de7ef4a45193a36
Jack <jackforez@gmail.com>
2023-03-04 10:49:32 +0700

    v

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -3,15 +3,23 @@ import { ktsConfig } from "../../ultis/config";
 import { Link } from "react-router-dom";
 const Category = () => {
   return (
-    <div className="max-w-screen-xl mx-auto text-center mt-3  bg-green-200 w-full overflow-hidden flex text-xs md:text-base gap-2 justify-around flex-nowrap">
+    <div className="max-w-screen-xl mx-auto text-center mt-3  bg-green-200 w-full overflow-hidden flex text-xs md:text-base gap-2 justify-around">
       {ktsConfig.categories.map((i, index) => {
         return (
           <Link
-            className="p-3 font-semibold hover:bg-green-500 block"
+            className="p-3 font-semibold hover:bg-green-500 flex gap-2 items-center flex-col w-full"
             to=""
             key={index}
           >
-            <p> {i.title}</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+              viewBox="0 0 512 512"
+              className="w-8 h-8"
+            >
+              <path d={i.path}></path>
+            </svg>
+            <p className="hidden md:block">{i.title}</p>
           </Link>
         );
       })}
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Footer, Header, Navbar, Promotion } from "../components";
-// import "./Contact.css";
-// import '../assets/css/base.css';
+import logo from "../assets/imgs/logo.png";
 
 const Contact = () => {
   return (
@@ -10,80 +9,145 @@ const Contact = () => {
       <Header />
       <Navbar />
       <div className="max-w-screen-xl bg-white mx-auto">
-        <div class="main">
-          <div class="section-content">
-            <div class="grid">
-              <div class="contact-content">
-                <div class="grid__row">
-                  <div class="contact-content-introduce">
-                    <h3>GREEN FOOD</h3>
-                    <p>
-                      Mọi thắc mắc quý khách vui lòng liên hệ tới chúng tôi
-                      thông qua thông tin bên dưới hoặc quý khách có thể điền
-                      thông tin ở form bên cạnh. Chúng tôi sẽ liên hệ giải đáp
-                      thắc mắc tới quý khách một cách sớm nhất.
-                    </p>
-                    <p>
-                      <i class="fa fa-map-marker" aria-hidden="true"></i> Địa
-                      chỉ : FLC Star 418 Quang Trung, La Khê, Hà Đông
-                    </p>
-                    <p>
-                      <i class="fa fa-envelope" aria-hidden="true"></i> Email :
-                      greenfood@gmail.com
-                    </p>
-                    <p>
-                      <i class="fa fa-phone" aria-hidden="true"></i> Điện thoại:
-                      0387969609
-                    </p>
-                    {/* <p><img decoding="async" loading="lazy" class="alignnone size-full wp-image-400"
-                            src={tenanh} alt="" width="232" height="89"/></p> */}
-                  </div>
-                  <div class="contact-content-information">
-                    <span>
-                      <input
-                        type="text"
-                        name=""
-                        id=""
-                        placeholder="Họ và tên..."
-                      />
-                      <span></span>
-                    </span>
-                    <span>
-                      <input
-                        type="email"
-                        name=""
-                        id=""
-                        placeholder="Địa chỉ email..."
-                      />
-                      <span></span>
-                    </span>
-                    <span>
-                      <input
-                        type="tel"
-                        name=""
-                        id=""
-                        placeholder="Số điện thoại..."
-                      />
-                      <span></span>
-                    </span>
-                    <span>
-                      <textarea
-                        name=""
-                        id=""
-                        cols="40"
-                        rows="10"
-                        placeholder="Nhập nội dung liên hệ..."
-                      ></textarea>
-                    </span>
-                    <input
-                      type="submit"
-                      value="GỬI LIÊN HỆ"
-                      class="btn btn--primary"
-                    />
-                  </div>
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2">
+            <img src={logo} alt="" />
+            <ul className="flex flex-col gap-2">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-8 h-8 -ml-1.5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                  />
+                </svg>
+
+                <span>
+                  Địa chỉ: Tầng 2 - KTS Home, Nguyễn Văn Linh, An Dương, Hải
+                  Phòng
+                </span>
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                  />
+                </svg>
+                <span>Email: hotro@sale168.vn</span>
+              </li>
+              <li className="flex gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                  />
+                </svg>
+
+                <span>Điện thoại: 0788.300.894</span>
+              </li>
+            </ul>
+            <img
+              src="https://green.web5phut.com/wp-content/uploads/2021/10/Untitled-7.png"
+              alt=""
+              className=" w-1/3 object-contain"
+            />
+          </div>
+          <div className="w-full md:w-1/2">
+            <form className="w-full flex flex-col gap-2">
+              <div className="mt-4">
+                <label htmlFor="name" className="font-semibold">
+                  Họ và tên
+                </label>
+                <input
+                  type="text"
+                  placeholder="Họ và tên ..."
+                  name="name"
+                  id="name"
+                  className="border-grey-light block w-full rounded border p-2 focus:border-primary focus:outline-none"
+                  required="abc"
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+              </div>
+              <div className="mt-4">
+                <label htmlFor="phone" className="font-semibold">
+                  Số điện thoại
+                </label>
+                <input
+                  type="text"
+                  placeholder="Số điện thoại ..."
+                  name="phone"
+                  id="phone"
+                  className="border-grey-light block w-full rounded border p-2 focus:border-primary focus:outline-none"
+                  required="abc"
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+              </div>
+              <div className="mt-4">
+                <label htmlFor="email" className="font-semibold">
+                  Địa chỉ email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Địa chỉ email ..."
+                  name="email"
+                  id="email"
+                  className="border-grey-light block w-full rounded border p-2 focus:border-primary focus:outline-none"
+                  required="abc"
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+              </div>
+              <div className="mt-4">
+                <label htmlFor="content" className="font-semibold">
+                  Nội dung
+                </label>
+                <textarea
+                  placeholder="Nội dung liên hệ ..."
+                  name="content"
+                  id="content"
+                  className="border-grey-light block w-full rounded border p-2 focus:border-primary focus:outline-none"
+                  required="abc"
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full rounded bg-primary
+                 px-5 py-3 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-primary"
+                  >
+                    Gửi liên hệ cho chúng tôi
+                  </button>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
