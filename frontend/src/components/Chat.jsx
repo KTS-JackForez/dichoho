6324c7ff6a095086b920d607d7c517816d96aa71
JackForez <jackforez@gmail.com>
2023-03-23 10:57:10 +0700

    t

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -12,7 +12,7 @@ const Chat = (props) => {
     <div className="bg-white max-w-md w-full shadow-md rounded fixed bottom-0 right-0 overflow-hidden z-30">
       <section className="">
         <div className="flex justify-between">
-          <span className="px-3 py-3">Sale168.com</span>
+          <span className="px-3 py-3 text-primary">Sale168.com</span>
           <button
             className="p-3 border-l bg-primary text-white"
             onClick={() => {
@@ -36,7 +36,7 @@ const Chat = (props) => {
           </button>
         </div>
       </section>
-      <div className="h-96 py-2 px-2.5 bg-gray-100 my-auto">
+      <div className="h-96 py-2 px-2.5 bg-gray-100 my-auto shadow-inner">
         Không có tin nhắn. Khi bạn nhắn tin, tin nhắn sẽ hiển thị tại đây.
       </div>
       <div className="flex justify-between px-4 gap-2 py-4">
