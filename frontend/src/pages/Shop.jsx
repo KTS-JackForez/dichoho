6d247239dd838491d200f3e5de364ddc82456173
JackForez <jackforez@gmail.com>
2023-03-27 12:01:26 +0700

    t

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -56,11 +56,28 @@ const Shop = () => {
             />
           </div>
           <div className="w-1/3 px-4 text-start space-y-4">
-            <h3 className="uppercase font-semibold text-xl">
-              {shopInfo.displayName}
-            </h3>
+            <div className="flex items-center space-x-4 pl-0.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
+              <h3 className="uppercase font-semibold text-xl">
+                {shopInfo.displayName}
+              </h3>
+            </div>
+
             <button
-              className="flex items-center gap-2 px-4 py-2 bg-primary rounded hover:bg-green-700"
+              className="flex items-center gap-2 pl-0.5 pr-4 py-2 bg-primary rounded hover:bg-green-700"
               onClick={() => {
                 if (!currentUser) {
                   return navigate("/login");
@@ -84,7 +101,7 @@ const Shop = () => {
               </svg>
               <span>Nhắn tin cho Shop </span>
             </button>
-            <div className="space-x-4">
+            <div className="space-x-4 pl-0.5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
