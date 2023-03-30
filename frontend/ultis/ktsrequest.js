88a6edbbfa5334ceefd8d82c2561655ab854cf1b
JackForez <jackforez@gmail.com>
2023-03-30 20:46:10 +0700

    v

--- frontend/ultis/ktsrequest.js
+++ frontend/ultis/ktsrequest.js
@@ -2,15 +2,9 @@ import axios from "axios";
 
 const ktsRequest = axios.create({
   // online
-<<<<<<< HEAD
-  // baseURL: "https://api.sale168.com/api",
+  baseURL: "https://api.sale168.com/api",
   //local
-  baseURL: "http://localhost:9000/api",
-=======
-   baseURL: "https://api.sale168.com/api",
-  //local
-  //baseURL: "http://localhost:9000/api",
->>>>>>> 7efe25ffa70a818be10c501cbae74c4118dc62f0
+  // baseURL: "http://localhost:9000/api",
 });
 
 export default ktsRequest;
