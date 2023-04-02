e63a83563c42ba62baa7653cdc269bf99889efe2
JackForez <jackforez@gmail.com>
2023-04-02 20:41:49 +0700

    v

--- frontend/src/components/Lastest.jsx
+++ frontend/src/components/Lastest.jsx
@@ -3,6 +3,7 @@ import ItemCard from "./ItemCard";
 import { ToastContainer, toast } from "react-toastify";
 import ktsRequest from "../../ultis/ktsrequest";
 import icon_ns from "../assets/imgs/icon_ns.webp";
+import { Link } from "react-router-dom";
 const Lastest = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -12,7 +13,7 @@ const Lastest = () => {
         setData(res.data);
       } catch (err) {
         err.response
-          ? toast.error(err.response.data.message)
+          ? toast.error(err.response.data)
           : toast.error("Network Error!");
       }
     };
@@ -26,7 +27,10 @@ const Lastest = () => {
             <img src={icon_ns} alt="" />
             <h3 className="uppercase font-semibold ">mới nhất</h3>
           </div>
-          <div className="flex items-center pr-4">
+          <Link
+            to={`/products`}
+            className="flex items-center pr-4 hover:text-primary"
+          >
             Xem tất cả
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -34,7 +38,7 @@ const Lastest = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-4 h-4"
+              className="w-4 h-4 ml-2"
             >
               <path
                 strokeLinecap="round"
@@ -42,7 +46,7 @@ const Lastest = () => {
                 d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
               />
             </svg>
-          </div>
+          </Link>
         </div>
         <div className="gap-2 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 justify-center grid-rows-2 py-2 w-full">
           {data.map((p, i) => {
