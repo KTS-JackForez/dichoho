ad94ade8db2724210ee988368fb27d70bec0c82b
JackForez <jackforez@gmail.com>
2023-04-28 11:48:51 +0700

    v

--- admin/src/pages/Layout.jsx
+++ admin/src/pages/Layout.jsx
@@ -29,7 +29,7 @@ const Layout = () => {
       <Sidebar />
       <div className="bg-gray-200 flex-1 h-screen">
         <Header />
-        <div className="h-[90vh] overflow-y-auto">
+        <div className="h-[90vh]">
           <Routes>
             <Route index element={<Home />} />
             <Route path="san-pham">
