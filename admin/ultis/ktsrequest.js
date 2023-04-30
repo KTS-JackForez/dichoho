201a0871c231934814d6ccdcf7ae57c61396ceee
Jack <jackforez@gmail.com>
2023-05-01 05:48:08 +0700

    v

--- admin/ultis/ktsrequest.js
+++ admin/ultis/ktsrequest.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const ktsRequest = axios.create({
   // online
-  // baseURL: "https://api.sale168.vn/api",
+  baseURL: "https://api.sale168.vn/api",
 
   //local
-  baseURL: "http://localhost:9000/api",
+  // baseURL: "http://localhost:9000/api",
 });
 
 export default ktsRequest;
