88e04187ca6a2280e3a2071690e20cb78e6ae285
Jack <jackforez@gmail.com>
2023-06-10 03:13:33 +0700

    v

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Footer, Header, Navbar, Promotion } from "../components";
-// import "./About.css";
-// import "../assets/css/base.css";
+import banner from "../assets/imgs/bn.png";
 const About = () => {
   return (
     <div className="bg-gray-100">
@@ -9,155 +8,68 @@ const About = () => {
       <Header />
       <Navbar />
       <div className="max-w-screen-lg bg-white mx-auto px-5 py-7">
-        <div class="main">
-          <div class="introduce">
-            <div class="introduce-main grid">
-              <div class="introduce-content">
-                <p>
-                  <b>
-                    Thương mại điện tử (E-Commerce) là hình thái hoạt động kinh
-                    doanh bằng các phương pháp điện tử; là việc trao đổi “thông
-                    tin” kinh doanh thông qua các phương tiện công nghệ điện
-                    tử.&nbsp;
-                  </b>
-                </p>
-                <p>
-                  <strong>1. Khái niệm&nbsp;Thương mại điện tử</strong>
-                </p>
-                <p>
-                  Công nghệ tiên tiến hiện nay giúp doanh nghiệp biến Website
-                  của mình thành những siêu thị hàng hóa trên Internet, biến
-                  người mua thực sự trở thành những người chủ với toàn quyền
-                  trong việc chọn lựa sản phẩm, tìm kiếm thông tin, so sánh giá
-                  cả, đặt mua hàng, ký kết hợp đồng với hệ thống tính toán tiền
-                  tự động, rõ ràng, trung thực.
-                </p>
-                <p>
-                  <a href="https://tmdt.web5phut.com/wp-content/uploads/2022/03/slider_2.jpg">
-                    <img
-                      decoding="async"
-                      class="size-medium wp-image-534 aligncenter"
-                      src="https://tmdt.web5phut.com/wp-content/uploads/2022/03/slider_2-800x344.jpg"
-                      alt=""
-                      width="800"
-                      height="344"
-                    />
-                  </a>
-                </p>
-                <p>
-                  <strong>
-                    <em>
-                      Thương mại điện tử&nbsp;(E-Commerce) là hình thái hoạt
-                      động kinh doanh bằng các phương pháp điện tử; là việc trao
-                      đổi “thông tin” kinh doanh thông qua các phương tiện công
-                      nghệ điện tử.
-                    </em>
-                  </strong>
-                </p>
-                <p>
-                  Hiện nay có rất nhiều cách hiểu khác nhau về&nbsp;Thương mại
-                  điện tử. Nhiều người hiểu&nbsp;Thương mại điện tử&nbsp;là bán
-                  hàng trên mạng, trên Internet. Một số ý kiến khác lại cho
-                  rằng&nbsp;Thương mại điện tử&nbsp;là làm thương mại bằng điện
-                  tử. Những cách hiểu này đều đúng theo một góc độ nào đó nhưng
-                  chưa nói lên được phạm vi rộng lớn củaThương mại điện tử.
-                </p>
-                <p>
-                  Theo khái niệm này,&nbsp;Thương mại điện tử&nbsp;không chỉ là
-                  bán hàng trên mạng hay bán hàng trên Internet mà là hình thái
-                  hoạt động kinh doanh bằng các phương pháp điện tử. Hoạt động
-                  kinh doanh bao gồm tất cả các hoạt động trong kinh doanh như
-                  giao dịch, mua bán, thanh toán, đặt hàng, quảng cáo và kể cả
-                  giao hàng. Các phương pháp điện tử ở đây không chỉ có Internet
-                  mà bao gồm việc sử dụng các phương tiện công nghệ điện tử như
-                  điện thoại, máy FAX, truyền hình và mạng máy tính (trong đó có
-                  Internet).&nbsp;Thương mại điện tử&nbsp;cũng bao hàm cả việc
-                  trao đổi thông tin kinh doanh thông qua các phương tiện công
-                  nghệ điện tử. Thông tin ở đây không chỉ là những số liệu hay
-                  văn bản, tin tức mà nó gồm cả hình ảnh, âm thanh và phim
-                  video.
-                </p>
-                <p>Các phương tiện điện tử trong&nbsp;Thương mại điện tử</p>
-                <p>+ Điện thoại</p>
-                <p>+ Máy FAX</p>
-                <p>+ Truyền hình</p>
-                <p>+ Hệ thống thanh toán điện tử</p>
-                <p>+ Intranet / Extranet</p>
-                <p>Mạng toàn cầu Internet / World Wide Web</p>
-                <p>Các hình thức hoạt động&nbsp;Thương mại điện tử</p>
-                <p>+ Thư tín điện tử (E-mail)</p>
-                <p>+ Thanh toán điện tử</p>
-                <p>+ Trao đổi dữ liệu điện tử (EDI)</p>
-                <p>+ Trao đổi số hoá các dung liệu</p>
-                <p>+ Mua bán hàng hoá hữu hình</p>
-                <p>
-                  Thương mại điện tử&nbsp;hiện nay đã trở nên khá quen thuộc và
-                  trở thành một môi trường thương mại không thể thiếu được trong
-                  đời sống kinh tế xã hội phát triển. Ở Việt Nam,&nbsp;Thương
-                  mại điện tử&nbsp;đang được các doanh nghiệp từng bước áp dụng
-                  vào hoạt động sản xuất kinh doanh của mình.&nbsp;Thương mại
-                  điện tử&nbsp;có những thế mạnh vượt trội mà không một loại
-                  hình kinh doành nào khác có được.
-                </p>
-                <p>
-                  <strong>
-                    2. Các ưu điểm của Website&nbsp;Thương mại điện tử
-                  </strong>
-                </p>
-                <p>
-                  Ưu điểm tuyệt đối của Thương mại Điện tử là cho phép người sử
-                  dụng thực hiện các hoạt động kinh doanh ngay lập tức trên quy
-                  mô toàn cầu, từ việc quảng cáo công ty, tiếp thị sản phẩm, đàm
-                  phán và đặt hàng cho đến các khâu thanh toán, giữ liên hệ với
-                  khách hàng và hỗ trợ sau bán hàng. Bởi vì:
-                </p>
-                <p>
-                  <strong>
-                    2.1.&nbsp;Thương mại điện tử&nbsp;giúp người bán
-                  </strong>
-                </p>
-                <p>
-                  •Tiếp thị hiệu quả sản phẩm và dịch vụ của mình ra khắp thế
-                  giới
-                </p>
-                <p>
-                  •Tạo kênh bán hàng trực tiếp tới khách hàng với quy mô rộng,
-                  tốc độ nhanh và chi phí giảm rất nhiều so với các kênh bán
-                  hàng truyền thống khác
-                </p>
-                <p>•Mở ra khả năng xuất khẩu hàng ra nước ngoài</p>
-                <p>
-                  •Đơn giản hóa được các thủ tục hành chính, các công việc giấy
-                  tờ, tăng hiệu quả giao dịch thương mại
-                </p>
-                <p>
-                  •Với Website&nbsp;Thương mại điện tử, doanh nghiệp tạo cho
-                  mình khả năng kinh doanh liên tục 24/24 giờ, liên tục 07 ngày
-                  trong tuần với chi phí rất thấp. Không cần nhân viên giám sát
-                  khách hàng như tại các siêu thị bình thường, không cần bỏ tiền
-                  thuê địa điểm bán hàng, không cần hệ thống kiểm tra, giới
-                  thiệu sản phẩm, không cần hệ thống tính tiền,… Tất cả đều được
-                  Website làm tự động, rất nhanh chóng và với độ chính xác tuyệt
-                  đối.
-                </p>
-                <p>
-                  •Tại cùng 1 thời điểm, Website&nbsp;Thương mại điện tử&nbsp;có
-                  thể phục vụ hàng triệu lượt người mua hàng ở khắp nơi trên thế
-                  giới với các yêu cầu rất khác nhau về thông tin sản phẩm,
-                  chủng loại sản phẩm, giá cả, hình ảnh, chất lượng, mẫu mã,…
-                </p>
-                <p>
-                  •Thông tin, giá cả sản phẩm được cập nhật, thay đổi một cách
-                  tức thời theo sự biến động của thị trường.
-                </p>
-                <p>
-                  Website Thương mại Điện tử đem lại khả năng kinh doanh mới cho
-                  doanh nghiệp: “Kinh doanh ngay cả khi bạn đang ngủ”.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <img
+          src={banner}
+          alt=""
+          className="mx-auto h-full w-full object-cover"
+        />
+        <p className="indent-8 mt-3">
+          Dichoho.top là một giải pháp của Sàn thương mại điện tử{" "}
+          <a href="https://sale168.vn" className="italic underline">
+            sale168.vn
+          </a>{" "}
+          chuyên về cung cấp nông sản sạch và các sản phẩm OCOP của thành phố
+          Hải Phòng. Chúng tôi đặt mục tiêu quảng bá và thúc đẩy nông sản tại
+          địa phương, đồng thời nâng cao nhận thức về nông sản sạch và bền vững
+          trong cộng đồng.
+        </p>
+        <p>
+          Sứ mệnh của chúng tôi là quảng bá, giới thiệu và phát triển các sản
+          phẩm nông sản tại Hải Phòng. Chúng tôi đặc biệt quan tâm đến nông sản
+          sạch, an toàn và bền vững, đồng thời cũng tạo điều kiện cho các sản
+          phẩm OCOP của địa phương có cơ hội tiếp cận thị trường rộng lớn hơn.
+        </p>
+        <p>
+          Một trong những chức năng chính của chuyên trang là dịch vụ "Đi chợ
+          hộ", nơi chúng tôi cung cấp các sản phẩm nông sản và giao hàng đến tận
+          nơi, đúng giờ và với mức phí tối ưu. Điều này đáp ứng tiêu chí và yêu
+          cầu của người tiêu dùng, đảm bảo rằng sản phẩm được cung cấp là "Tiện
+          - Tươi - Sạch - Rẻ". Đi chợ hộ là một dịch vụ cần thiết và hữu ích cho
+          những người bận rộn và muốn tiết kiệm thời gian mua sắm.
+        </p>
+        <p>
+          Mục tiêu của chuyên trang Dichoho.top là trở thành một tấm “bản đồ số”
+          giới thiệu và cung cấp nông sản an toàn và sản phẩm OCOP của Hải Phòng
+          cho mọi người. Ngoài ra chúng tôi tin rằng việc quảng bá nông sản sạch
+          và OCOP sẽ mở ra các hình thức du lịch sinh thái kết hợp với trải
+          nghiệm nông nghiệp thú vị và hiệu quả trên địa bàn thành phố Hải
+          Phòng.
+        </p>
+        <p>
+          Với sứ mệnh và mục tiêu của mình, Dichoho.top cam kết cung cấp cho
+          khách hàng những sản phẩm nông sản chất lượng và chính sách dịch vụ
+          tốt nhất. Chúng tôi luôn nỗ lực để đáp ứng nhu cầu mua sắm và tạo niềm
+          tin cho khách hàng với sự chất lượng, tin cậy và sự hài lòng tối đa.
+        </p>
+        <p>
+          Chúng tôi cam kết tạo điều kiện thuận lợi cho các nhà sản xuất, các
+          chủ thể, HTX và doanh nghiệp nhỏ để tiếp cận thị trường trực tuyến và
+          quảng bá sản phẩm của mình trên chuyên trang dichoho.top.
+        </p>
+        <p>
+          Chúng tôi tin rằng sự hợp tác và phát triển bền vững với các đối tác,
+          khách hàng và cộng đồng là chìa khóa để thành công. Chúng tôi luôn
+          lắng nghe ý kiến đóng góp của khách hàng và sẵn lòng cải tiến để mang
+          đến trải nghiệm mua sắm tốt nhất cho mọi người.
+        </p>
+        <p>
+          Hãy tham gia và đồng hành cùng{" "}
+          <a href="https://sale168.vn" className="italic underline">
+            sale168.vn
+          </a>{" "}
+          ngay hôm nay để khám phá thế giới nông sản sạch và sản phẩm OCOP độc
+          đáo của chúng tôi.
+        </p>
       </div>
       <Footer />
     </div>
