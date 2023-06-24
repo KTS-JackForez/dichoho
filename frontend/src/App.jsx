284106231679439f1080207877728f45d8fff7ea
Jack <jackforez@gmail.com>
2023-06-24 13:07:19 +0700

    v

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect, lazy } from "react";
+import { useEffect, lazy, Suspense } from "react";
 
 import { Home } from "./pages";
 const News = lazy(() => import("./pages/News"));
-const Product = lazy(() => import("./pages"));
-const Login = lazy(() => import("./pages"));
-const Register = lazy(() => import("./pages"));
-const NotFound = lazy(() => import("./pages"));
-const Contact = lazy(() => import("./pages"));
-const About = lazy(() => import("./pages"));
-const Cart = lazy(() => import("./pages"));
-const Post = lazy(() => import("./pages"));
-const Products = lazy(() => import("./pages"));
-const Dashboard = lazy(() => import("./pages"));
-const Shop = lazy(() => import("./pages"));
+const Product = lazy(() => import("./pages/Product"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Contact = lazy(() => import("./pages/Contact"));
+const About = lazy(() => import("./pages/About"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Post = lazy(() => import("./pages/Post"));
+const Products = lazy(() => import("./pages/Products"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Shop = lazy(() => import("./pages/Shop"));
 
 import { useSelector } from "react-redux";
 import ktsRequest from "../ultis/ktsrequest";
@@ -36,35 +36,37 @@ function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="news">
-            <Route index element={<News />} />
-            <Route path=":postId" element={<Post />} />
+      <Suspense>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="news">
+              <Route index element={<News />} />
+              <Route path=":postId" element={<Post />} />
+            </Route>
+            <Route path="contact" element={<Contact />} />
+            <Route path="about" element={<About />} />
+            <Route path="products">
+              <Route index element={<Products />} />
+              <Route path=":productId" element={<Product />} />
+            </Route>
+            <Route path="abc" element={<Products />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="shop/:shopId" element={<Shop />} />
+            <Route
+              path="dashboard/*"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route path="contact" element={<Contact />} />
-          <Route path="about" element={<About />} />
-          <Route path="products">
-            <Route index element={<Products />} />
-            <Route path=":productId" element={<Product />} />
-          </Route>
-          <Route path="abc" element={<Products />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="shop/:shopId" element={<Shop />} />
-          <Route
-            path="dashboard/*"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
