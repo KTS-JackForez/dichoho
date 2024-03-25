797bf765a7b4840de886b9c244908fa4a2838e43
Jack <jackforez@gmail.com>
2024-03-25 13:08:14 +0700

    thay đổi midbaner thành dạng slide.

--- frontend/src/components/MidBanner.jsx
+++ frontend/src/components/MidBanner.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import bn5 from "../assets/imgs/Banner5.webp";
+import bn6 from "../assets/imgs/Banner6.webp";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper";
+import "swiper/swiper-bundle.css";
+import { Link } from "react-router-dom";
+SwiperCore.use([Autoplay]);
 const MidBanner = () => {
   return (
     <div className="mt-3">
-      <img src={bn5} alt="" className="mx-auto object-cover object-center" />
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 5000 }}
+        className="max-w-screen-xl mx-auto text-center mt-3  bg-green-200 w-full overflow-hidden flex text-xs md:text-base gap-2 justify-around relative"
+      >
+        <SwiperSlide className="">
+          <Link to="/news/65ec18afa3aa6d6523034444">
+            <img
+              src={bn5}
+              alt=""
+              className="mx-auto object-cover object-center"
+            />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <Link to="/news/65ec1e1ea3aa6d65230344ae">
+            <img
+              src={bn6}
+              alt=""
+              className="mx-auto object-cover object-center"
+            />
+          </Link>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
